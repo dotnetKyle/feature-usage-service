980e2120dc38149a227fff66ccefd70845a14b97
Kyle <dotnetKyle@gmail.com>
2021-06-25 18:26:49 -0400

    checking in changes, started on a few more database calls, this is a good start

--- src/FeatureUsage/DAO/FeatureUsageReportsRepoAsync.cs
+++ src/FeatureUsage/DAO/FeatureUsageReportsRepoAsync.cs
@@ -15,7 +15,7 @@ namespace FeatureUsage.DAO
 {
     public interface IFeatureUsageReportsRepoAsync
     {
-        Task CalculateEachFeatureCountAsync(DateTime startTime, DateTime stopTime, CancellationToken cancellationToken = default(CancellationToken));
+        Task<List<FeatureTotalCount>> CalculateEachFeatureCountAsync(DateTime startTime, DateTime stopTime, CancellationToken cancellationToken = default(CancellationToken));
     }
     public class FeatureUsageReportsRepoAsync : IFeatureUsageReportsRepoAsync
     {
@@ -40,47 +40,13 @@ namespace FeatureUsage.DAO
         /// <param name="startTime"></param>
         /// <param name="stopTime"></param>
         /// <param name="cancellationToken"></param>
-        public async Task CalculateEachFeatureCountAsync(DateTime startTime, DateTime stopTime, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task<List<FeatureTotalCount>> CalculateEachFeatureCountAsync(
+            DateTime startTime, 
+            DateTime stopTime, 
+            CancellationToken cancellationToken = default(CancellationToken))
         {
-            try
-            {
-                startTime = DateTime.UtcNow.AddHours(-1);
-                stopTime = DateTime.UtcNow.AddHours(1);
-
-                var pipeline = new EmptyPipelineDefinition<FeatureUsageRecord>()
-                    .Unwind<FeatureUsageRecord, FeatureUsageRecord, UnwoundFeatureUsageRecord>(
-                        f => f.UsageData
-                    )
-                    .Match(
-                        f => 
-                               f.UsageData.UsageDtgUTC >= startTime 
-                            && f.UsageData.UsageDtgUTC < stopTime
-                    )
-                    .Group(e => e.FeatureName, 
-                        e => new FeatureTotalCount
-                        { 
-                            FeatureName = e.Key,
-                            UsageCount = e.Count()
-                        }
-                    )
-                    .Sort(
-                        new SortDefinitionBuilder<FeatureTotalCount>()
-                            .Descending(e => e.UsageCount)
-                    );
-
-                var cursor = await _collection.AggregateAsync(pipeline, null, cancellationToken);
-
-                var list = await cursor.ToListAsync(cancellationToken);
-            }
-            catch(Exception ex)
-            {
-                System.Diagnostics.Debug.WriteLine("Error:");
-                System.Diagnostics.Debug.WriteLine(ex);
-            }
-        }
-
-        /*
-            Report to calculate the number of times each feature has been used.
+            /* Report to calculate the number of times each feature has been used.
+            JSON:
 
             db.featureUsage.aggregate([
                 { $unwind: "$usageData" },
@@ -90,35 +56,91 @@ namespace FeatureUsage.DAO
                 },
                 { $sort: { usageCount: -1 } }
             ])
-            Report to calculate the number of times a specific feature has been used.
+             */
 
+            var pipeline = new EmptyPipelineDefinition<FeatureUsageRecord>()
+                .Unwind<FeatureUsageRecord, FeatureUsageRecord, UnwoundFeatureUsageRecord>(
+                    f => f.UsageData
+                )
+                .Match(f => 
+                           f.UsageData.UsageDtgUTC >= startTime 
+                        && f.UsageData.UsageDtgUTC < stopTime
+                )
+                .Group(e => e.FeatureName, 
+                    e => new FeatureTotalCount
+                    { 
+                        FeatureName = e.Key,
+                        UsageCount = e.Count()
+                    }
+                )
+                .Sort(
+                    Builders<FeatureTotalCount>.Sort.Descending(e => e.UsageCount)
+                );
+
+            var cursor = await _collection.AggregateAsync(pipeline, null, cancellationToken);
+
+            var list = await cursor.ToListAsync(cancellationToken);
+
+            return list;
+        }
+
+        public async Task<List<FeatureTotalCount>> CalculateSpecificFeatureCountAsync(
+            string featureName, 
+            DateTime startTime, 
+            DateTime stopTime, 
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            /* Report to calculate the number of times a specific feature has been used.
+             * 
+            JSON
             db.featureUsage.aggregate([
-                { $match: { featureName:"Main Window|Long Running Service" } },
                 { $unwind: "$usageData" },
+                { $match: { featureName:"Main Window|Long Running Service" } },
                 { $group: { _id: "$featureName",
                     usageCount: { $sum: 1 }
                   }
                 }
             ])
+            */
 
-
-            Benchmark Data Reports
-
-
-            Report to calculate the average benchmark for all features that are benchmarked.
-
-            db.featureUsage.aggregate([
-                { $project: {
-                        _id: 0,
-                        user: "$userName",
-                        featureName: "$featureName",
-                        benchmarkMs: "$usageData.benchmarkMs"
+            var pipeline = new EmptyPipelineDefinition<FeatureUsageRecord>()
+                .Unwind(e => e.UsageData, new AggregateUnwindOptions<UnwoundFeatureUsageRecord>())
+                .Match(e => 
+                       e.FeatureName == featureName
+                    && e.UsageData.UsageDtgUTC >= startTime
+                    && e.UsageData.UsageDtgUTC < stopTime
+                )
+                .Group(e => e.FeatureName,
+                    e => new FeatureTotalCount
+                    {
+                        FeatureName = e.Key,
+                        UsageCount = e.Count()
                     }
-                }, 
+                )
+                .Sort(
+                    Builders<FeatureTotalCount>.Sort.Descending(e => e.UsageCount)
+                );
+
+            var cursor = await _collection.AggregateAsync(pipeline, null, cancellationToken);
+
+            var list = await cursor.ToListAsync(cancellationToken);
+
+            return list;
+        }
+
+        public async Task<List<FeatureBenchmarkAggregate>> BenchmarkAverageForAllBenchmarkedFeaturesAsync(
+            DateTime startTime, 
+            DateTime stopTime, 
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            /* Report to calculate the average benchmark for all features that are benchmarked.
+             * 
+            JSON
+            db.featureUsage.aggregate([
+                { $unwind: "$usageData" },                
                 { $match: {
                         "benchmarkMs": { $exists: true, $ne: [] }
                 }}, 
-                { $unwind: { path: "$benchmarkMs" } }, 
                 { $group: {
                         _id: "$featureName",
                         benchmarkAverage: {
@@ -126,8 +148,48 @@ namespace FeatureUsage.DAO
                         }
                 }}
             ])
-            Report to calculate the average benchmark time for a specific feature.
+            */
 
+            var pipeline = new EmptyPipelineDefinition<FeatureUsageRecord>()
+                // might have to aggregate first
+                .Unwind(e => e.UsageData, new AggregateUnwindOptions<UnwoundBenchmarkUsageRecord>())
+                .Match(
+                    Builders<UnwoundBenchmarkUsageRecord>.Filter.And(
+                        // only if benchmark exists
+                        Builders<UnwoundBenchmarkUsageRecord>.Filter.Exists(e => e.BenchmarkMs),
+                        Builders<UnwoundBenchmarkUsageRecord>.Filter.Gte(e => e.UsageData.UsageDtgUTC, startTime),
+                        Builders<UnwoundBenchmarkUsageRecord>.Filter.Lt (e => e.UsageData.UsageDtgUTC, stopTime)
+                    )
+                )
+                .Group(e => e.FeatureName,
+                    e => new FeatureBenchmarkAggregate
+                    {
+                        FeatureName = e.Key,
+                        // average the benchmarks for this period
+                        BenchmarkMs = e.Average(x => x.BenchmarkMs)
+                    }
+                )
+                .Sort(
+                    // sort slowest features up top
+                    Builders<FeatureBenchmarkAggregate>.Sort.Descending(e => e.BenchmarkMs)
+                );
+
+            var cursor = await _collection.AggregateAsync(pipeline, null, cancellationToken);
+
+            var list = await cursor.ToListAsync(cancellationToken);
+
+            return list;
+        }
+
+        public async Task<List<FeatureBenchmarkAggregate>> BenchmarkAverageForSpecificFeatureAsync(
+            string featureName, 
+            DateTime startTime, 
+            DateTime stopTime, 
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            /* Report to calculate the average benchmark time for a specific feature.
+             * 
+            JSON
             db.featureUsage.aggregate([
                 { $match: { featureName:"Main Window|Long Running Service" } },
                 { $project: {
@@ -144,7 +206,37 @@ namespace FeatureUsage.DAO
                   }
                 }
             ])
-         */
+            */
+
+            var pipeline = new EmptyPipelineDefinition<FeatureUsageRecord>()
+                .Unwind(e => e.UsageData, new AggregateUnwindOptions<UnwoundBenchmarkUsageRecord>())
+                .Match(
+                    Builders<UnwoundBenchmarkUsageRecord>.Filter.And(
+                        // only if benchmark exists
+                        Builders<UnwoundBenchmarkUsageRecord>.Filter.Exists(e => e.BenchmarkMs),
+                        Builders<UnwoundBenchmarkUsageRecord>.Filter.Gte(e => e.UsageData.UsageDtgUTC, startTime),
+                        Builders<UnwoundBenchmarkUsageRecord>.Filter.Lt(e => e.UsageData.UsageDtgUTC, stopTime)
+                    )
+                )
+                .Group(e => e.FeatureName,
+                    e => new FeatureBenchmarkAggregate
+                    {
+                        FeatureName = e.Key,
+                        // average the benchmarks for this period
+                        BenchmarkMs = e.Average(x => x.BenchmarkMs)
+                    }
+                )
+                .Sort(
+                    // sort slowest features up top
+                    Builders<FeatureBenchmarkAggregate>.Sort.Descending(e => e.BenchmarkMs)
+                );
+
+            var cursor = await _collection.AggregateAsync(pipeline, null, cancellationToken);
+
+            var list = await cursor.ToListAsync(cancellationToken);
+
+            return list;
+        }
     }
 
     public class UnwoundFeatureUsageRecord
@@ -174,6 +266,27 @@ namespace FeatureUsage.DAO
         public override string ToString()
         {
             return $"{FeatureName} ({UsageCount})";
+        }
+    }
+    public class UnwoundBenchmarkUsageRecord : UnwoundFeatureUsageRecord
+    {
+        [BsonIgnoreIfNull]
+        [BsonElement("benchmarkMs")]
+        public int BenchmarkMs { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FeatureName} {UserName} Ms:[{BenchmarkMs}]";
+        }
+    }
+    public class FeatureBenchmarkAggregate
+    {
+        public string FeatureName { get; set; }
+        public double BenchmarkMs { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FeatureName} ({BenchmarkMs}ms)";
         }
     }
 
